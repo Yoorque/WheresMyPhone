712ba62ba26cf43ea0ff655beecca6d1913fabc4
Dusan Juranovic <juranovicd@gmail.com>
2018-10-11 19:27:44 +0200

    Last somewhat stable version... Still needs a lot of refactoring into rx and MVVM.

--- WheresMyPhone/Controller/ViewController.swift
+++ WheresMyPhone/Controller/ViewController.swift
@@ -102,11 +102,7 @@ class ViewController: UIViewController {
         
         //Add Mock Devices
         deviceManager.addDevice(mock.mock1)
-        deviceManager.addDevice(mock.mock2)
-        deviceManager.addDevice(mock.mock1)
-        deviceManager.addDevice(mock.mock2)
-        deviceManager.addDevice(mock.mock1)
-        
+        deviceManager.addDevice(mock.mock2)        
     }
     
     func showProgress(_ progress: Double) {
@@ -254,5 +250,46 @@ class ViewController: UIViewController {
         }
     }
 }
-//MARK: Extensions in Extensions folder
 
+//MARK: - tableViewDelegate and tableViewDataSource -
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    //DataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return deviceManager.devices.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! DeviceCell
+        cell.nameLabel.text = deviceManager.devices.value[indexPath.row].name
+        cell.movesLabel.text = "\(deviceManager.devices.value[indexPath.row].coordinates.value.count)"
+        return cell
+    }
+    
+    
+    //Delegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedRow = indexPath.row
+        trackCoordinateObservable.onNext(deviceManager.devices.value[indexPath.row].coordinates.value.last!)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete", handler: { _,_,_ in
+            self.selectedRow = 0
+            self.deviceManager.devices.value.remove(at: indexPath.row)
+            // tableView.deleteRows(at: [indexPath], with: .left)
+            self.tableView.reloadData()
+        })
+        let config = UISwipeActionsConfiguration(actions: [deleteAction])
+        return config
+    }
+}
